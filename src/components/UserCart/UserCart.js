05a4d505deb010a063866d47b00203d47d738758
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-03-05 14:40:24 -0800

    mild changes

--- src/components/UserCart/UserCart.js
+++ src/components/UserCart/UserCart.js
@@ -64,11 +64,11 @@ export default class UserCart extends React.Component
                        <td> <input type="number" placeholder={singleitem.qty} onChange={this.handleChange} name={singleitem.name}/></td>
                        <td>{singleitem.name} </td>
                     <td>{singleitem.single_price}</td>
-                    <td>{Math.round(singleitem.item_price)}</td>
+                    <td>{(singleitem.item_price)}</td>
                     </tr>)
                 })}
                 </tbody></table>
-                GRAND TOTAL = ${Math.round(this.state.total)}
+                GRAND TOTAL = ${(this.state.total)}
             </div>
         )
     }
