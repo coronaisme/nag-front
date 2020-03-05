898f31aa55d39b59f9537db62b3a2887775c843d
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-03-05 09:20:49 -0800

    cart calculations

--- src/components/UserCart/UserCart.js
+++ src/components/UserCart/UserCart.js
@@ -11,9 +11,34 @@ export default class UserCart extends React.Component
     render()
     {
         return(
-            <div> Props : {this.props.cart[0].split(",")[0]}
-                {console.log(this.props)}
+            <div>
+
+                {this.props.cart.length > 0 ? 
+                
+                    this.props.cart.map(item => {
+                    console.log(item)
+                    var name = item.split(",")[0]
+                    var price =item[item.split(",").length -1]
+                    debugger
+                    // return <input type="number"/>  <p>{name }</p>
+                    console.log(name,price)
+                })
+                
+                :
+                <div>
+                Your Cart is Empty. 
+                </div>
+                }
             </div>
         )
     }
 }
+
+// {
+//     "cart": [
+//       "Coconut Juice,,$3.00",
+//       "Green Mussel,Cooked green mussel with ponzu sauce, topped with tobiko and green onion. Served cold.,$5.00",
+//       "Pineapple Fried Rice,Wok fried rice with choice of meat, egg, tomatoes, onion, pineapple, raisin, and Cashew nuts.,$9.95",
+//       "Ika Sugatayaki,Grilled whole squid with Japanese mayo or spicy and sour sauce.,$12.00"
+//     ]
+//   }
